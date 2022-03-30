f18093b60c988e0945575a77e070535cf7835756
kaho-lc <lc1536328699@163.com>
2022-03-30 14:16:51 +0800

    movie recommender

---
+++ Recommender/DataLoader/src/main/scala/com/lc/DataLoader.scala
@@ -0,0 +1,202 @@
+package com.lc
+
+import org.apache.http.HttpHost
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.client.indices.GetIndexRequest
+import org.elasticsearch.client.{RequestOptions, RestClient, RestHighLevelClient}
+
+import java.util.Properties
+
+//数据加载模块
+object DataLoader {
+
+  //  定义数据路径常量
+  val movieDataPath = "D:\\project\\MovieRecommendSystem\\Recommender\\DataLoader\\src\\main\\resources\\movies.csv"
+  val ratingDataPath = "D:\\project\\MovieRecommendSystem\\Recommender\\DataLoader\\src\\main\\resources\\ratings.csv"
+  val tagsDataPath = "D:\\project\\MovieRecommendSystem\\Recommender\\DataLoader\\src\\main\\resources\\tags.csv"
+
+  val Mysql_MovieTable_Name = "movie"
+  val Mysql_RatingTable_Name = "rating"
+  val Mysql_TagTable_Name = "tag"
+  val ES_MOVIE_INDEX = "Movie"
+
+  //定义相关的配置
+  val config = Map(
+    "spark.cores" -> "local[*]",
+    "es.httpHosts" -> "hadoop102:9200",
+    "es.transportHosts" -> "hadoop102:9300",
+    "es.index" -> "recommender",
+    "es.cluster.name" -> "cluster-es",
+
+    "mysql.url" -> "jdbc:mysql://localhost:3306/recommendsystem",
+    "mysql.driver" -> "com.mysql.jdbc.Driver",
+    "mysql.user" -> "root",
+    "mysql.password" -> "000000",
+  )
+
+  def main(args: Array[String]): Unit = {
+    //创建一个sparkconfig对象
+    val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("DataLoader")
+
+    //操作ES的相关配置
+    //    sparkConf.set("es.nodes", "localhost")
+    //    sparkConf.set("es.port", "9200")
+    //    sparkConf.set("es.index.auto.create", "true")
+    //    sparkConf.set("es.write.operation", "index")
+
+    //创建sparksession
+    val spark: SparkSession = new SparkSession.Builder().config(sparkConf).getOrCreate()
+
+    //引入词包，用于将RDD转换成DataFrame
+    import spark.implicits._
+
+
+    //加载数据
+    val movieRDD = spark.sparkContext.textFile(movieDataPath);
+
+    //转换成DataFrame
+    val movieDF: DataFrame = movieRDD.map(
+      line => {
+        val attr: Array[String] = line.split("\\^") //需要对^进行转义使用\,但是还需要对\进行转义
+        //        封装成样例类
+        Movie(attr(0).toInt, attr(1).trim(), attr(2).trim(), attr(3).trim(), attr(4).trim(), attr(5).trim(), attr(6).trim(), attr(7).trim(), attr(8).trim(), attr(9).trim())
+      }
+    ).toDF()
+
+
+    val ratingRDD = spark.sparkContext.textFile(ratingDataPath)
+
+    val ratingDF: DataFrame = ratingRDD.map(
+      line => {
+        val attr: Array[String] = line.split(",")
+        Rating(attr(0).toInt, attr(1).toInt, attr(2).toDouble, attr(3).toInt)
+      }
+    ).toDF()
+
+    val tagRDD = spark.sparkContext.textFile(tagsDataPath);
+
+    val tagDF: DataFrame = tagRDD.map(
+      line => {
+        val attr: Array[String] = line.split(",")
+        Tag(attr(0).toInt, attr(1).toInt, attr(2), attr(3).toInt)
+      }
+    ).toDF()
+
+    //todo 数据预处理 : 将movie对应的tag信息添加进去 ， 加一列数据 tag1|tag2|tag3
+
+    /**
+     * 数据格式
+     * mid , tags
+     *
+     * tags:tag1|tag2
+     */
+    val newTag = tagDF.groupBy($"mid")
+      .agg(concat_ws("|", collect_set($"tag")).as("tags"))
+      .select("mid", "tags")
+
+    //对newTag和movie做一个join操作，将数据合并在一起
+    //左外连接
+    val movieWithTags: DataFrame = movieDF.join(newTag, Seq("mid"), "left")
+
+    //将数据保存到mysql中
+    //    storeDataToMysql(movieDF, ratingDF, tagDF);
+
+    //保存数据到es
+    storeDataToES(movieWithTags);
+
+    spark.stop();
+  }
+
+  //保存数据到Mysql
+  def storeDataToMysql(MovieDF: DataFrame, RatingDF: DataFrame, tagDF: DataFrame) = {
+    //    连接数据库的相关配置
+    val prop: Properties = new Properties()
+    prop.put("driver", config("mysql.driver"))
+    prop.put("user", config("mysql.user"))
+    prop.put("password", config("mysql.password"))
+    //    将dataframe写入到mysql
+    MovieDF.write.jdbc(config("mysql.url"), Mysql_MovieTable_Name, prop)
+
+    RatingDF.write.jdbc(config("mysql.url"), Mysql_RatingTable_Name, prop)
+
+    tagDF.write.jdbc(config("mysql.url"), Mysql_TagTable_Name, prop)
+  }
+
+  //保存数据到ElasticSearch
+  def storeDataToES(movieWithTags: DataFrame) = {
+
+    val client: RestHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200)))
+    val request: IndexRequest = new IndexRequest()
+
+    request.index("recommender")
+    val indexRequest = new GetIndexRequest("recommender")
+
+    //判断索引是否存在
+    val exist: Boolean = client.indices().exists(indexRequest, RequestOptions.DEFAULT)
+
+    //如果索引存在
+    if (exist) {
+      import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
+      val deleteIndexRequest = new DeleteIndexRequest()
+      deleteIndexRequest.indices("recommender")
+      //删除索引
+      client.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+
+    } else {
+      import org.elasticsearch.client.indices.CreateIndexRequest //否则
+      //创建索引
+      client.indices().create(new CreateIndexRequest("recommender"), RequestOptions.DEFAULT);
+    }
+
+    //ES的相关配置
+    val options = Map(
+      //如果没有index,则新建
+      "es.index.auto.create" -> "true",
+      "es.nodes.wan.only" -> "true",
+      //安装es的服务器节点
+      "es.nodes" -> "localhost",
+      //es的端口
+      "es.port" -> "9200",
+      "es.mapping.id" -> "mid"
+    )
+
+    movieWithTags.write.format("org.elasticsearch.spark.sql").options(options).mode("overwrite")
+      .save("recommender" + "/" + ES_MOVIE_INDEX)
+  }
+}
+
+//定义一些样例类
+
+/**
+ * movie数据集
+ * 260                                            电影id:mid
+ * ^Star Wars: Episode IV - A New Hope (1977)     电影名称:name
+ * ^Princess Leia is captured and held            详情描述:descri
+ * ^121minutes                                    时长:timelog
+ * ^September 21, 2004                            发行时间:issue
+ * ^1977                                          拍摄时间:shoot
+ * ^English                                       语言:language
+ * ^Action|Adventure|Sci-Fi                       类型:genres
+ * ^Mark Hamill|Harrison Ford                     演员表:actors
+ * ^George Lucas                                  导演:directors
+ * */
+case class Movie(mid: Int, name: String, descri: String, timelog: String, issue: String, shoot: String, language: String,
+                 genres: String, actors: String, directors: String)
+
+
+/*
+* 1,2968,1.0,1260759200  uid:用户id ， 电影id:mid , 评分:score , 评分时间:timestamp
+* */
+
+
+case class Rating(uid: Int, mid: Int, score: Double, timestamp: Int)
+
+
+/**
+ * 15,1955,dentist,1193435061    uid:用户id ， 电影id:mid , 标签:tag , 评分时间:timestamp
+ */
+case class Tag(uid: Int, mid: Int, tag: String, timestamp: Int)
---
+++ Recommender/StatisticsRecommender/src/main/scala/com/lc/StatisticsRecommender.scala
@@ -0,0 +1,111 @@
+package com.lc
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object StatisticsRecommender {
+
+  import org.apache.spark.sql.DataFrame
+
+  import java.util.Properties
+
+  //定义表名
+  val Mysql_MovieTable_Name = "movie"
+  val Mysql_RatingTable_Name = "rating"
+
+  //统计表的名称
+  val RATE_MORE_MOVIES = "RateMoreMovies" //历史热门统计
+  val RATE_MORE_RECENTLY_MOVIES = "RateMoreRecentlyMovies" //最近热门统计
+  val AVERAGE_MOVIES = "AverageMovies" //平均评分统计
+  val GENRES_TOP_MOVIES = "GenresTopMovies" //类别top10统计
+
+  //定义相关的配置
+  val config = Map(
+    "spark.cores" -> "local[*]",
+    "mysql.url" -> "jdbc:mysql://localhost:3306/recommendsystem",
+    "mysql.driver" -> "com.mysql.jdbc.Driver",
+    "mysql.user" -> "root",
+    "mysql.password" -> "000000",
+  )
+
+  val prop = new Properties()
+  prop.put("driver", config("mysql.driver"))
+  prop.put("user", config("mysql.user"))
+  prop.put("password", config("mysql.password"))
+
+  def main(args: Array[String]): Unit = {
+    import org.apache.spark.rdd.RDD
+    val conf: SparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("StatisticsRecommender")
+    val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+
+    //读取数据转换成DataFrame
+    val ratingDF: DataFrame = spark.sqlContext.read.jdbc(config("mysql.url"), Mysql_RatingTable_Name, prop);
+    val movieDF: DataFrame = spark.sqlContext.read.jdbc(config("mysql.url"), Mysql_MovieTable_Name, prop);
+
+    //创建名为ratings的临时视图
+    ratingDF.createOrReplaceTempView("ratings")
+
+    //todo ：不同的统计推荐结果
+    //1.历史热门电影统计:根据所有历史评分数据，计算历史评分次数最多的电影。
+    //mid , count
+    val rateMoreMoviesDF: DataFrame = spark.sql("select mid , count(mid) as count from ratings group by mid")
+    //    storeDFtoMySql(rateMoreMoviesDF, RATE_MORE_MOVIES)
+
+
+    //2.最近热门电影统计:根据评分，按"yyyyMM"月为单位计算最近时间的月份里面评分数最多的电影集合。
+    //创建日期格式化工具
+    val simpleDataFormat: SimpleDateFormat = new SimpleDateFormat("yyyyMM")
+
+    //注册一个udf函数将时间戳转换成想要的yyyyMM格式
+    spark.udf.register("changeDate", (timestamp: Int) => simpleDataFormat.format(new Date(timestamp * 1000L)).toInt)
+
+    //对原始数据做预处理，去掉uid
+    val ratingOfYearMonth: DataFrame = spark.sql("select mid , score , changeDate(timestamp) as yearmonth from ratings")
+    ratingOfYearMonth.createOrReplaceTempView("ratingOfMonth")
+
+    //从临时表中查找电影在各个月份的评分统计，mid , count , yearmonth
+    val rateMoreRecentlyMovies: DataFrame = spark.sql("select mid , count(mid) as count , yearmonth from  ratingOfMonth group by mid , yearmonth order by yearmonth desc , count desc")
+    //存储到MySQL中
+    //    storeDFtoMySql(rateMoreRecentlyMovies, RATE_MORE_RECENTLY_MOVIES)
+
+
+    //3.电影平均得分统计:根据历史数据中所有用户对电影的评分，周期性的计算每个电影的平均得分。
+    //mid , avg
+    val averageMoviesDF: DataFrame = spark.sql("select mid , avg(score) as avg from ratings group by mid")
+
+    //存储到MySQL中
+    //    storeDFtoMySql(averageMoviesDF, AVERAGE_MOVIES)
+
+
+    //4.每个类别优质电影统计:根据提供的所有电影类别，分别计算每种类型的电影集合中评分最高的 10 个电影。
+    //定义出所有的类别
+    List("Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Foreign", "History", "Horror", "Music", "Mystery", "Romance", "Science", "Tv", "Thriller", "War", "Western")
+    
+
+
+
+    //关闭资源
+    spark.stop();
+  }
+
+  //存储DF到MySQL中
+  def storeDFtoMySql(df: DataFrame, tableName: String): Unit = {
+    df.write.jdbc(config("mysql.url"), tableName, prop)
+  }
+
+  //定义样例类
+  case class Movie(mid: Int, name: String, descri: String, timelog: String, issue: String, shoot: String, language: String,
+                   genres: String, actors: String, directors: String)
+
+  case class Rating(uid: Int, mid: Int, score: Double, timestamp: Int)
+
+  //定义基准推荐对象
+  case class Recommendation(mid: Int, score: Double)
+
+  //定义电影类别top10推荐样例类
+  case class GenresRecommendation(genres: String, recs: Seq[Recommendation])
+
+}
